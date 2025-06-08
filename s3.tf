ac632cf886d7b267f60b97937e75eb99ff117bcc
Harshit Ghagre <117425012+harshitghagre@users.noreply.github.com>
2025-06-08 18:44:50 +0530

    [terraform:  s3]  add S3 bucket resource with versioning and environment tag

---
+++ s3.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "devops_assoc_bucket" {
+  bucket = "devops-assoc-bucket"
+
+  tags = {
+    Environment = "DevOpsTest"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.devops_assoc_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
